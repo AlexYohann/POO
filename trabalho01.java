be8704bd287d44723788bc7c75e6771145708d8a
AlexYohann <103403099+AlexYohann@users.noreply.github.com>
2022-04-19 16:19:30 -0300

    trabalhaço

--- trabalho01.java
+++ trabalho01.java
@@ -1,19 +1,23 @@
-public class trabalho01{
+public class trabalho01 {
+    static int[][] a1 = new int[][]{
+        new int[]{1,2,3},
+        new int[]{4,5,6},
+        new int[]{7,8,9}
+    };
 
-    public static void main(String[] args){}
-    int[][] a1 = new int[][]{
-    new int[]{1,2,3},
-    new int[]{4,5,6},
-    new int[]{7,8,9}};
-            public static int[][] rotacionarMatrizHorario(int[][] matriz) {
-                int largura = matriz.length;
-                int altura = matriz[0].length;
-                int[][] ret = new int[altura][largura];
-                for (int i=0; i<altura; i++) {
-                    for (int j=0; j<largura; j++) {
-                        ret[i][j] = matriz[largura - j - 1][i];
-                    }
-                 }
-                return ret;
+    public static void rodaroda(int[][] matriz) {
+        int largura = matriz.length;
+        int altura = matriz[0].length;
+        int[][] ret = new int[altura][largura];
+        for (int i=0; i<altura; i++) {
+            for (int j=0; j<largura; j++) {
+                ret[i][j] = matriz[largura - j - 1][i];
             }
+            System.out.println(ret[i][0] + "" + ret[i][1] + "" + ret[i][2]);
         }
+    }
+
+    public static void main(String[] args){
+       rodaroda(a1);
+    }
+}
